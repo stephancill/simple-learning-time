b6fba297a71ce1c082fdca064cfda314ce5baeae
Stephan Cilliers <stephanus.cilliers@gmail.com>
2016-03-21 00:00:35 +0200

    Added new hill to background

--- SimpleLearningTime/BackgroundManager.swift
+++ SimpleLearningTime/BackgroundManager.swift
@@ -12,25 +12,32 @@ import SpriteKit
 class BackgroundManager {
 
     var spriteMovingBackground = SKSpriteNode(imageNamed: "imageBackground")
-    var spriteBackgroundColour = SKSpriteNode(color: UIColor(), size: UIScreen.mainScreen().bounds.size)
+    var spriteBackgroundHill = SKSpriteNode(imageNamed: "imageBackgroundHill")
     var spriteUtilitiesBackground = SKSpriteNode()
+    var spriteBackgroundColor = SKSpriteNode(color: UIColor(), size: UIScreen.mainScreen().bounds.size)
 
     func initElements (frameSize: CGSize, scalar:Float, scene:SKScene, center:CGPoint, time: (CGFloat, CGFloat)=(CGFloat(0), CGFloat(0))) {
         
         let mid = CGPoint(x: frameSize.width * 0.5, y: frameSize.height * 0.5)
         
-        spriteMovingBackground.zPosition = -1
+        spriteUtilitiesBackground = SKSpriteNode(color: UIColor(white: 1, alpha: 0.2), size: CGSize(width: frameSize.width/35*4*4.5, height: frameSize.height*2))
+        spriteUtilitiesBackground.zPosition = -1
+        scene.addChild(spriteUtilitiesBackground)
+        
+        spriteBackgroundHill.zPosition = -2
+        spriteBackgroundHill.anchorPoint = CGPoint(x: 0.5, y: 0.0)
+        spriteBackgroundHill.position = CGPoint(x:frameSize.width/2, y: 0)
+        scene.addChild(spriteBackgroundHill)
+        
+        spriteMovingBackground.zPosition = -3
         spriteMovingBackground.anchorPoint = CGPoint(x:0.5, y:0.5)
         spriteMovingBackground.position = CGPoint(x:center.x, y: center.y/2)
         spriteMovingBackground.size = CGSize(width: spriteMovingBackground.size.width * CGFloat(scalar), height: spriteMovingBackground.size.height * CGFloat(scalar))
         scene.addChild(spriteMovingBackground)
         
-        spriteBackgroundColour.position = mid
-        spriteBackgroundColour.zPosition = -2
-        scene.addChild(spriteBackgroundColour)
-        
-        spriteUtilitiesBackground = SKSpriteNode(color: UIColor(white: 1, alpha: 0.2), size: CGSize(width: frameSize.width/35*4*4.5, height: frameSize.height*2))
-        scene.addChild(spriteUtilitiesBackground)
+        spriteBackgroundColor.zPosition = -4
+        spriteBackgroundColor.position = mid
+        scene.addChild(spriteBackgroundColor)
         
     }
     
@@ -39,7 +46,7 @@ class BackgroundManager {
         // Background's rotation is half of hour's rotation
         spriteMovingBackground.zRotation = cm.clockElements[cm.hourNodeID].zRotation/2
         
-        // Background colour
+        // Background Color
         adjustBackgroundColor()
 
     }
@@ -53,8 +60,12 @@ class BackgroundManager {
     func adjustBackgroundColor (hue: CGFloat=0.62, saturation: CGFloat=1, alpha: CGFloat=1){
         
         var brightness = abs((sqrt(pow(abs((cm.clockElements[cm.hourNodeID].zRotation / CGFloat(math.clockHourIntervalConst)) % cm.hourMod)-12,2))/12)%1-1)
-        if (cm.clockElements[cm.hourNodeID].zRotation / CGFloat(math.clockHourIntervalConst) % cm.hourMod == 0) {brightness = 0.00001}
-        spriteBackgroundColour.color = UIColor(hue: hue-brightness/10, saturation: saturation, brightness: brightness, alpha: alpha)
         
+        if (cm.clockElements[cm.hourNodeID].zRotation / CGFloat(math.clockHourIntervalConst) % cm.hourMod == 0) {brightness = 0.00001}
+        
+        spriteBackgroundColor.color = UIColor(hue: hue-brightness/10, saturation: saturation, brightness: brightness, alpha: alpha)
+        
+        spriteBackgroundHill.color = spriteBackgroundColor.color
+        spriteBackgroundHill.colorBlendFactor = 0.9
     }
 }

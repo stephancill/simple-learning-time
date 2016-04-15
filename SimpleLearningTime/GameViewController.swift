b87f7cfb85ca0e0171dcb45d4063b831680132f5
Stephan Cilliers <stephanus.cilliers@gmail.com>
2016-04-15 17:08:09 +0200

    Change support iPhone and iPad screen sizes (iPhone still stretched)

--- SimpleLearningTime/GameViewController.swift
+++ SimpleLearningTime/GameViewController.swift
@@ -10,24 +10,32 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
-
+    
+    var deviceType = "iPad"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if let scene = GameScene(fileNamed:"GameScene") {
             // Configure the view.
             let skView = self.view as! SKView
             skView.showsFPS = false
             skView.showsNodeCount = false
-
+            
             /* Sprite Kit applies additional optimizations to improve rendering performance */
             skView.ignoresSiblingOrder = false
             
             /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
+            if (deviceType == "iPhone") {
+                scene.scaleMode = .Fill
+            } else {
+                scene.scaleMode = .AspectFill
+            }
             
+            /* Present the scene */
             skView.presentScene(scene)
         }
+
     }
 
     override func shouldAutorotate() -> Bool {
@@ -36,8 +44,11 @@ class GameViewController: UIViewController {
 
     override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
         if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+            deviceType = "iPad"
             return .Landscape
+            
         } else {
+            deviceType = "iPhone"
             return .Landscape
         }
     }

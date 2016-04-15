0e0ced2fbc6c6b3eb77e8b9f6786f134f0f1f2b4
Stephan Cilliers <stephanus.cilliers@gmail.com>
2016-04-15 15:42:23 +0200

    Implemented travis

---
+++ SimpleLearningTime/SelfTestManager.swift
@@ -0,0 +1,20 @@
+//
+//  SelfTestManager.swift
+//  SimpleLearningTime
+//
+//  Created by Stephan Cilliers on 2016/04/07.
+//  Copyright © 2016 Stephan Cilliers. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class SelfTestManager {
+
+    init() {
+    
+    }
+    
+    
+    
+}

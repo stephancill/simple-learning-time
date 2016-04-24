29299d6230d5fbf1f3841d98e5ffd6a2a4e89a82
Stephan Cilliers <stephanus.cilliers@gmail.com>
2016-04-24 23:18:59 +0200

    Removed redundant comment

--- SimpleLearningTime/HelpInfoManager.swift
+++ SimpleLearningTime/HelpInfoManager.swift
@@ -145,13 +145,7 @@ class HelpInfoManager {
                 node.anchorPoint.x = 0
                 node.removeFromParent()
             }
-        
-//            spriteHelp1224Hour.position.x = bgm.spriteUtilitiesBackground.size.width
-//            spriteHelp1224Hour.anchorPoint.x = 0
-//            spriteHelpRandomTime.position.x = bgm.spriteUtilitiesBackground.size.width
-//            spriteHelpCurrentTime.position.x = bgm.spriteUtilitiesBackground.size.width
-//            spriteHelpSelfTest.position.x = bgm.spriteUtilitiesBackground.size.width
-//            spriteHelpDigitalToggle.position.x = bgm.spriteUtilitiesBackground.size.width
+            
         }
         
     }

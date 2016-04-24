cdaf460efb8ab635a264849d815a83c664126a6f
Stephan Cilliers <stephanus.cilliers@gmail.com>
2016-04-24 23:19:34 +0200

    Initial commit for DeviceConfigurations class

---
+++ SimpleLearningTime/DeviceConfigurations.swift
@@ -0,0 +1,35 @@
+//
+//  DeviceConfigurations.swift
+//  SimpleLearningTime
+//
+//  Created by Stephan Cilliers on 2016/04/24.
+//  Copyright © 2016 Stephan Cilliers. All rights reserved.
+//
+
+import Foundation
+
+class DeviceConfigurations {
+    // Device-specific constants
+    
+    
+    // iPhone
+    /* Sizes */
+    /* Buttons */
+
+        /*
+         -
+         -
+         -
+         */
+
+    // iPad
+    /* Sizes */
+    /* Buttons */
+
+        /*
+         -
+         -
+         -
+         */
+
+}
